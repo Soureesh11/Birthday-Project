e6595bf1c8df8a15e3eb3067292f6aa8efdf9fa2
Soureesh11 <106656783+Soureesh11@users.noreply.github.com>
2022-06-06 19:17:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg,function(img)')
+    {
+        block_imgage_object = Img;{(
+            block_imgage_object.scaleToWidth(700);
+            block_imgage_object.scaleToHeight(510);
+            block_imgage_object.set ({
+            top:0
+            left:0
+         });
+         canvas.add(block_imgage_object);
+        )};
+        
+     
+
+
+    
+        
+
+        
+        
+
+        
+    }
+	
+}
+
+function playSound(){
+	x.play();
+}
